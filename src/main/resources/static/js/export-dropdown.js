ee34ccfbcd1ff7da329e05cb11832cbe81d44bb6
Arnaldo Piccinelli <arnaldo.piccinelli@newscyclesolutions.com>
2014-08-29 20:32:09 -0400

    Dandelion Datatables export links as a dropdown menu.

--- src/main/resources/static/js/export-dropdown.js
+++ src/main/resources/static/js/export-dropdown.js
@@ -1,23 +1,11 @@
-//$( '.dataTables_export' ).ready(function() {
-//    console.log("ready!");
-//    console.log($('.dataTables_export').attr('class'));
-//    console.log($('a:contains("XLS")').attr('href'));
-//});
-
 var checkExist = setInterval(function() {
     if ($('.dataTables_export').length) {
-        console.log("Exists!");
-        console.log($('a:contains("XLS")').attr('href'));
+        $('#exportAsCsv').attr('href', $('.dataTables_export a:contains("CSV")').attr('href'));        
+        $('#exportAsPdf').attr('href', $('.dataTables_export a:contains("PDF")').attr('href'));        
+        $('#exportAsXml').attr('href', $('.dataTables_export a:contains("XML")').attr('href'));        
+        $('#exportAsXls').attr('href', $('.dataTables_export a:contains("XLS")').attr('href'));        
+        $('#exportAsXlsx').attr('href', $('.dataTables_export a:contains("XLSX")').attr('href'));        
+        
         clearInterval(checkExist);
-    } else {
-    	console.log("Not yet!");
     }
 }, 100);
-
-/*
- * <a href="/owners/list.html?lastName=&amp;dtt=f&amp;dti=owners&amp;dtf=xls&amp;dtp=y&amp;dandelionAssetFilterState=false">XLS</a>
- * <a href="/owners/list.html?lastName=&amp;dtt=f&amp;dti=owners&amp;dtf=xlsx&amp;dtp=y&amp;dandelionAssetFilterState=false">XLSX</a>
- * <a href="/owners/list.html?lastName=&amp;dtt=f&amp;dti=owners&amp;dtf=pdf&amp;dtp=y&amp;dandelionAssetFilterState=false">PDF</a>
- * <a href="/owners/list.html?lastName=&amp;dtt=f&amp;dti=owners&amp;dtf=xml&amp;dtp=y&amp;dandelionAssetFilterState=false">XML</a>
- * <a href="/owners/list.html?lastName=&amp;dtt=f&amp;dti=owners&amp;dtf=csv&amp;dtp=y&amp;dandelionAssetFilterState=false">CSV</a>
- */
